3e3e625476d27f6849405050d9921612c39a4022
Kiselevich-Alex <54080508+Kiselevich-Alex@users.noreply.github.com>
2020-12-04 17:42:40 +0300

    added resizeable UI

--- ui.py
+++ ui.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtGui import QImage, QPixmap
-from PyQt5.QtWidgets import QAction, QFileDialog
+from PyQt5.QtWidgets import QAction, QFileDialog, QSizePolicy
 from PyQt5.QtCore import Qt
 import evaluate
 from evaluate import evaluate
@@ -11,6 +11,8 @@ from numpy import asarray
 
 class Ui_mainWindow(object):
     filename = ""
+    has_lr_image = False
+    has_upscale_image = False
 
     def setupUi(self, mainWindow):
         mainWindow.setObjectName("mainWindow")
@@ -18,18 +20,18 @@ class Ui_mainWindow(object):
         mainWindow.setMinimumSize(QtCore.QSize(800, 600))
         self.centralwidget = QtWidgets.QWidget(mainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.gridLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.gridLayoutWidget.setGeometry(QtCore.QRect(0, 0, 801, 581))
-        self.gridLayoutWidget.setObjectName("gridLayoutWidget")
-        self.gridLayout = QtWidgets.QGridLayout(self.gridLayoutWidget)
-        self.gridLayout.setContentsMargins(10, 10, 10, 10)
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.gridLayout = QtWidgets.QGridLayout()
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
         self.gridLayout.setObjectName("gridLayout")
-        self.label = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setFrameShape(QtWidgets.QFrame.Box)
         self.label.setText("")
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
         self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.horizontalLayout.addLayout(self.gridLayout)
         mainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(mainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
@@ -55,6 +57,29 @@ class Ui_mainWindow(object):
         self.menubar.addAction(upscale)
         self.menubar.addAction(save)
 
+    def resizeEvent(self, *args):
+        if self.has_lr_image:
+            if self.has_upscale_image:
+                # convert numpy array to Qimage
+                height, width, channel = self.upscale_image.shape
+                bytesPerLine = 3 * width
+                qImg = QImage(self.upscale_image.data, width, height, bytesPerLine, QImage.Format_RGB888)
+                print(1)
+            else:
+                # convert numpy array to Qimage
+                height, width, channel = self.lr_image.shape
+                bytesPerLine = 3 * width
+                qImg = QImage(self.lr_image.data, width, height, bytesPerLine, QImage.Format_RGB888)
+                print(2)
+
+            # convert Qimage to pixmap
+            self.pixmap = QPixmap(QPixmap.fromImage(qImg))
+            self.pixmap = self.pixmap.scaled(self.label.size(), Qt.KeepAspectRatio)
+
+            # output image on screen
+            self.label.setAlignment(Qt.AlignCenter)
+            self.label.setPixmap(self.pixmap)
+
     def open(self):
         options = QFileDialog.Options()
         filename, _ = QFileDialog.getOpenFileName(self, "Open Image", "", "PNG Files (*.png)", options=options)
@@ -65,12 +90,13 @@ class Ui_mainWindow(object):
             image = Image.open(self.filename)
 
             # convert image to numpy array
-            lr_image = asarray(image)
+            self.lr_image = asarray(image)
+            self.has_lr_image = True
 
             # convert numpy array to Qimage
-            height, width, channel = lr_image.shape
+            height, width, channel = self.lr_image.shape
             bytesPerLine = 3 * width
-            qImg = QImage(lr_image.data, width, height, bytesPerLine, QImage.Format_RGB888)
+            qImg = QImage(self.lr_image.data, width, height, bytesPerLine, QImage.Format_RGB888)
 
             # convert Qimage to pixmap
             self.pixmap = QPixmap(QPixmap.fromImage(qImg))
@@ -83,6 +109,7 @@ class Ui_mainWindow(object):
     def upscale(self):
         if len(self.filename) != 0:
             self.upscale_image = evaluate(self.filename)
+            self.has_upscale_image = True
 
             # convert numpy array to Qimage
             height, width, channel = self.upscale_image.shape
@@ -99,7 +126,8 @@ class Ui_mainWindow(object):
 
     def save(self):
         options = QFileDialog.Options()
-        filename, _ = QFileDialog.getSaveFileName(self, "Save Image", "upscale image.png", "PNG Files (*.png)", options=options)
+        filename, _ = QFileDialog.getSaveFileName(self, "Save Image", "upscale image.png", "PNG Files (*.png)",
+                                                  options=options)
         if filename:
             scipy.misc.imsave(filename, self.upscale_image)
 
